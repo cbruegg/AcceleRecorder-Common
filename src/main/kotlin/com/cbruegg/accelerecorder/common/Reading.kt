d5d28e3a83281cfcb46dd37e6eb6985d7b060a9a
Christian <c.brueggemann94@gmail.com>
2016-11-03 17:50:00 +0100

    Add validation method

--- src/main/kotlin/com/cbruegg/accelerecorder/common/Reading.kt
+++ src/main/kotlin/com/cbruegg/accelerecorder/common/Reading.kt
@@ -8,15 +8,19 @@ import java.io.FileWriter
 
 private val mapper = jacksonObjectMapper()
 
-data class Reading(val values: Map<String, Double>,
-                   val didVibrate: Boolean,
-                   val timestamp: Long,
-                   val source: Source,
-                   val sensor: Sensor,
-                   /**
-                    *  diff to add to the local time to get actual time of this value
-                    */
-                   val timeDiff: Int = 0) {
+data class Reading(
+        /**
+         * Set of keys is a function of source and sensor
+         */
+        val values: Map<String, Double>,
+        val didVibrate: Boolean,
+        val timestamp: Long,
+        val source: Source,
+        val sensor: Sensor,
+        /**
+         *  diff to add to the local time to get actual time of this value
+         */
+        val timeDiff: Int = 0) {
 
     enum class Source {
         LOCAL, PEBBLE, BAND
@@ -33,3 +37,15 @@ fun List<Reading>.writeAsJson(file: File) {
 }
 
 fun readReadings(json: String): List<Reading> = mapper.readValue(json)
+
+fun List<Reading>.validateValueKeys() {
+    val keysBySensorSource = mutableMapOf<Pair<Reading.Sensor, Reading.Source>, Set<String>>()
+    for (reading in this) {
+        val keys = keysBySensorSource.getOrPut(reading.sensor to reading.source) {
+            reading.values.keys
+        }
+        if (reading.values.keys != keys) {
+            throw IllegalArgumentException("$reading has invalid keys ${reading.values.keys}, expected $keys")
+        }
+    }
+}
